4fb7af6059377dee7761a94c8321194e1a6cef41
Fiodor Pranju <feodorpranju1@gmail.com>
2024-01-25 23:48:58 +0300

    initial packages and settings

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+import { router } from '@inertiajs/vue3';
+
+defineProps({
+    title: String,
+});
+
+const logout = () => {
+    router.post(route('logout'));
+};
+
+</script>
+
+<template>
+    <!-- Page Content -->
+    <main>
+        <slot />
+    </main>
+</template>
